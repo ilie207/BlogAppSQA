43dbcc19311f8005ced0793b887b94f39f69db24
Dilawar Dayal <dilawardayal@Dilawars-MacBook-Pro.local>
2025-01-07 17:06:06 +0000

    added stats from database posts

--- blog_app_sqa/src/app/createPost/page.js
+++ blog_app_sqa/src/app/createPost/page.js
@@ -1,12 +1,13 @@
 import React from "react";
 import CreatePost from "../components/CreatePost";
 import NavBar from "../components/NavBar";
+import Header from "../components/Header";
 
 export default function CreatePostPage() {
   return (
     <div>
+      <Header />
       <main>
-        <h1>Blog with Next JS</h1>
         <NavBar />
         <CreatePost />
       </main>
