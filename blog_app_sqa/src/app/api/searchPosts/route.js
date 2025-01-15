d0ba43d46134697946c4afc58cde785f9776fe30
Ilie202 <114577561+Ilie202@users.noreply.github.com>
2025-01-15 15:20:55 +0000

    rebased

--- blog_app_sqa/src/app/api/searchPosts/route.js
+++ blog_app_sqa/src/app/api/searchPosts/route.js
@@ -4,7 +4,6 @@ import pool from "../../../lib/db";
 
 export async function POST(req) {
   try {
-<<<<<<< HEAD
     const { getUser } = getKindeServerSession();
     const user = await getUser();
     const { title, content } = await req.json();
@@ -26,53 +25,6 @@ export async function GET() {
   try {
     const result = await pool.query('SELECT * FROM "BlogPosts"');
     return NextResponse.json(result.rows);
-=======
-    const { searchParams } = new URL(request.url);
-    const query = searchParams.get("query");
-    const sort = searchParams.get("sort");
-
-    let searchResults = [];
-    let allPosts = [];
-
-    // Build the ORDER BY clause based on sort parameter
-    const orderBy =
-      sort === "alphabet" ? "ORDER BY title ASC" : 'ORDER BY "createdAt" DESC';
-
-    if (query) {
-      // Search query for matching posts
-      const searchQuery = `
-        SELECT * FROM "BlogPosts" 
-        WHERE title ILIKE $1 
-        OR author ILIKE $1 
-        ${orderBy}
-      `;
-      const searchResult = await pool.query(searchQuery, [`%${query}%`]);
-      searchResults = searchResult.rows;
-
-      // Get remaining posts
-      const remainingQuery = `
-        SELECT * FROM "BlogPosts" 
-        WHERE id NOT IN (
-          SELECT id FROM "BlogPosts" 
-          WHERE title ILIKE $1 
-          OR author ILIKE $1
-        )
-        ${orderBy}
-      `;
-      const remainingResult = await pool.query(remainingQuery, [`%${query}%`]);
-      allPosts = remainingResult.rows;
-    } else {
-      // If no search query, get all posts
-      const allPostsQuery = `SELECT * FROM "BlogPosts" ${orderBy}`;
-      const result = await pool.query(allPostsQuery);
-      allPosts = result.rows;
-    }
-
-    return NextResponse.json({
-      searchResults,
-      allPosts,
-    });
->>>>>>> 92402c6 (added sort functionality)
   } catch (error) {
     console.error("Database error:", error);
     return NextResponse.json({ error: error.message }, { status: 500 });
